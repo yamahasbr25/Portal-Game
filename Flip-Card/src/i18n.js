977cd27d0815f52339f24782633d9f60058a4ba6
yamahasbr25 <yamahasbr25@gmail.com>
2025-01-28 10:49:08 +0700

    Add files via upload

---
+++ Flip-Card/src/i18n.js
@@ -0,0 +1,117 @@
+i18n = [
+    {
+        "id": "playnow",
+        "en": "Play Now",
+        "ja": "プレイする"
+    },
+    {
+        "id": "instructions",
+        "en": "Instructions",
+        "ja": "操作方法"
+    },
+    {
+        "id": "escinstruction",
+        "en": "Press [BACK] or [ESC] to abandon game.",
+        "ja": "ゲームを終了するには[BACK]または[ESC]を押してください。"
+    },
+    {
+        "id": "tutorial1",
+        "en": "Flip is a timed card memory game. Click the green cards to see what symbol they uncover and try to find the matching symbol underneath the other cards.",
+        "ja": "Flipはタイムリミットのカードメモリーゲームです。裏向きのカードをクリックすると、そのカードの記号が確認できます。"
+    },
+    {
+        "id": "tutorial2",
+        "en": "Uncover two matching symbols at once to eliminate them from the game.",
+        "ja": "同じ記号のカードをめくって、ゲームから排除。"
+    },
+    {
+        "id": "tutorial3",
+        "en": "Eliminate all cards as fast as you can to win the game. Have fun FLIPing!",
+        "ja": "ゲームに勝つためにできるだけ早くすべてのカードを排除する。"
+    },
+    {
+        "id": "casual",
+        "en": "Casual",
+        "ja": "初級"
+    },
+    {
+        "id": "medium",
+        "en": "Medium",
+        "ja": "中級"
+    },
+    {
+        "id": "hard",
+        "en": "Hard",
+        "ja": "上級"
+    },
+    {
+        "id": "bestcasual",
+        "en": "Best Casual:",
+        "ja": "初級ベスト"
+    },
+    {
+        "id": "bestmedium",
+        "en": "Best Medium:",
+        "ja": "中級ベスト"
+    },
+    {
+        "id": "besthard",
+        "en": "Best Hard:",
+        "ja": "上級ベスト"
+    },
+    {
+        "id": "totalflips",
+        "en": "Total Flips:",
+        "ja": "Flip（めくった）数"
+    },
+    {
+        "id": "matchedflips",
+        "en": "Matched Flips:",
+        "ja": "正解"
+    },
+    {
+        "id": "wrongflips",
+        "en": "Wrong Flips:",
+        "ja": "不正解"
+    },
+    {
+        "id": "youdidgreat",
+        "en": "You did great, you're the best.",
+        "ja": "素晴らしい！"
+    },
+    {
+        "id": "startagain",
+        "en": "Click or spacebar to start again",
+        "ja": "クリックまたはスペースキーでもう一度始める"
+    },
+    {
+        "id": "figures",
+        "en": "Figures: ",
+        "ja": "数字"
+    },
+    {
+        "id": "won",
+        "en": "WON",
+        "ja": "勝ち"
+    },
+    {
+        "id": "lost",
+        "en": "LOST",
+        "ja": "負け"
+    },
+    {
+        "id": "abandoned",
+        "en": "ABANDONED",
+        "ja": "終了"
+    },
+    {
+        "id": "clickp",
+        "en": "Click the P to get started",
+        "ja": "Pをクリックして始める"
+    },
+    {
+        "id": "didntflip",
+        "en": "Looks like you haven't FLIPped yet.",
+        "ja": "まだフリップしていないようです。"
+    }
+];
